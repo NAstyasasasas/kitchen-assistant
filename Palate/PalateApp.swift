26e6b8cdc8addc03989c12c17f0e6ba2ea9da0e9
NAstyasasasas <142779148+NAstyasasasas@users.noreply.github.com>
2026-04-02 21:24:17 +0300

    Initial Commit

---
+++ Palate/PalateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalateApp.swift
+//  Palate
+//
+//  Created by Анастасия on 02.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct PalateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
