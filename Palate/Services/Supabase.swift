9fbd6a7be13c7f1de231421202413838a94f585a
NAstyasasasas <142779148+NAstyasasasas@users.noreply.github.com>
2026-04-29 16:33:18 +0300

    feat: добавлен Supabase для хранения аватаров

---
+++ Palate/Services/Supabase.swift
@@ -0,0 +1,12 @@
+//
+//  Supabase.swift
+//  Palate
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+    supabaseURL: URL(string: "https://pvohfjuonfmuolqyofgv.supabase.co")!,
+    supabaseKey: "sb_publishable_Ae00TFFh-bfu1xRA2Riopg_1GCkZq69"
+)
